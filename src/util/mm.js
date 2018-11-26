c9d34212462ded1af1dd48cf4ec36c2f10d897d0
zhangyan <zhangyan@jindankeji.com>
2018-11-26 19:27:50 +0800

    a

--- src/util/mm.js
+++ src/util/mm.js
@@ -8,7 +8,7 @@
 'use strict';
 var Hogan = require('hogan.js');
 var conf = {
-    serverHost : '://www.qiongmaomall.top'
+    serverHost : 'http://www.qiongmaomall.top'
 };
 var _mm = {
     // 网络请求
